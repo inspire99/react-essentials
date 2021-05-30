5dcb04cffad2e6d430844470ac9ca0f03de0a787
TECHNO PARADIGMS <ganeshswamypillai@gmail.com>
2021-05-30 11:53:41 +0530

    Create Header, Main, Footer for restaurant page
    Create Header, Main, Footer for restaurant page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+
 import './App.css';
+import restaurant from './restaurant.jpg'
+
+
+function Header(props){
+  return(
+      <header>
+          <h1>{props.name}'s Kitchen</h1>
+      </header>
+  );
+}
+
+const dishes = [
+
+  "Chapathi and Paneer curry",
+  "Bread and jam",
+  "Idli and vada",
+  "Mutter paneer parotta"
+
+];
+
+
+// transformation function
+const dishObject = dishes.map((dish,i) => ({id:i,title:dish}));
+
+
+function Main(props){
+  return(
+    
+    <section>
+      <p>We serve the most {props.adjective} food around</p>
+      <img src={restaurant} height="200" alt="napkin and fork at a restaurant table"/>
+      <ul style={{textAlign: "left"}}>
+        {props.dishes.map((dish)=> <li key={dish.id}>{dish.title}</li>)}
+      </ul>
+    </section>
+  );
+}
+
+function Footer(props){
+  return(
+    <footer>
+      <p>Copyright {props.year}</p>
+    </footer>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Sai"/>
+      <Main adjective="amazing" dishes={dishObject}/>
+      
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <header>
+            <h1>Eve's Kitchen</h1>
+        </header>
+    );
+  }
+
+export default Header;
