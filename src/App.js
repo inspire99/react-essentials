ff887dc98b669e9233b776ad5b72ff5e9c5a8bcc
TECHNO PARADIGMS <ganeshswamypillai@gmail.com>
2021-05-30 22:37:37 +0530

    handle loading, failure, success, pending states of fetch api

--- src/App.js
+++ src/App.js
@@ -6,15 +6,24 @@ import './App.css';
 function App({login}) {
 
   const [data,setData] = useState(null);
+  const [loading,setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(
     () => {
+      if(!login)
+        return;
+      setLoading(true);
       fetch(`https://api.github.com/users/${login}`)
       .then((response) => response.json())
       .then(setData)
+      .then(setLoading(false))
+      .catch(setError);
     },[login]
   );
   
+  if(loading) return <h1>Loading ...</h1>
+
   if(data){
     return ( 
       <div>
