acee679c9f549416c286af8c7cd1fdd2af3f8df0
TECHNO PARADIGMS <ganeshswamypillai@gmail.com>
2021-05-30 23:00:27 +0530

    testing App component

--- src/App.js
+++ src/App.js
@@ -3,39 +3,14 @@ import './App.css';
 
 // https://api.github.com/users/inspire99
 
-function App({login}) {
+function App() {
 
-  const [data,setData] = useState(null);
-  const [loading,setLoading] = useState(false);
-  const [error, setError] = useState(null);
 
-  useEffect(
-    () => {
-      if(!login)
-        return;
-      setLoading(true);
-      fetch(`https://api.github.com/users/${login}`)
-      .then((response) => response.json())
-      .then(setData)
-      .then(setLoading(false))
-      .catch(setError);
-    },[login]
-  );
-  
-  if(loading) return <h1>Loading ...</h1>
-
-  if(data){
-    return ( 
-      <div>
-        <h1>{data.name}</h1>
-        <img alt={data.login} src={data.avatar_url}/>
-        <p>{data.location}</p>
-      </div>
-    );
-  }
 
   return(
-  <div>No User available</div>
+  <div>
+    <h1>Hello React testing library</h1>
+  </div>
   );
   
 }
---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import {render} from '@testing-library/react';
+import {React} from 'react';
+import App from './App';
+
+test("renders an h1", () =>{
+
+    const {getByText} = render(<App />);
+
+    const h1 = getByText(/Hello React testing library/);
+
+    expect(h1).toHaveTextContent("Hello React testing library");
+}
+)
