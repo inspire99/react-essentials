b7a88837f74cd94606a98217891850163c7d361d
TECHNO PARADIGMS <ganeshswamypillai@gmail.com>
2021-05-30 12:04:40 +0530

    intro to conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,60 +1,28 @@
 
 import './App.css';
-import restaurant from './restaurant.jpg'
 
 
-function Header(props){
+function SecretComponent(){
   return(
-      <header>
-          <h1>{props.name}'s Kitchen</h1>
-      </header>
+    <h1>Only authorized users can view secret information</h1>
   );
 }
 
-const dishes = [
-
-  "Chapathi and Paneer curry",
-  "Bread and jam",
-  "Idli and vada",
-  "Mutter paneer parotta"
-
-];
-
-
-// transformation function
-const dishObject = dishes.map((dish,i) => ({id:i,title:dish}));
-
-
-function Main(props){
+function RegularComponent(){
   return(
-    
-    <section>
-      <p>We serve the most {props.adjective} food around</p>
-      <img src={restaurant} height="200" alt="napkin and fork at a restaurant table"/>
-      <ul style={{textAlign: "left"}}>
-        {props.dishes.map((dish)=> <li key={dish.id}>{dish.title}</li>)}
-      </ul>
-    </section>
+    <h1>Everyone can see this component</h1>
   );
 }
 
-function Footer(props){
-  return(
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  );
-}
 
-function App() {
-  return (
-    <div className="App">
-      <Header name="Sai"/>
-      <Main adjective="amazing" dishes={dishObject}/>
-      
-      <Footer year={new Date().getFullYear()}/>
-    </div>
-  );
+function App(props) {
+  if(props.authorized){
+    return <SecretComponent />;
+  }
+  else{
+    return <RegularComponent />;
+  }
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,17 +4,12 @@ import './index.css';
 import App from './App';
 
 
-function AppTwo(){
-  return(
-    <h2>This is new App</h2>
-  );
-}
+
 
 ReactDOM.render(
-  <React.Fragment>
-  <App/>
-  <AppTwo />
-  </React.Fragment>,
+
+  <App authorized={true}/>
+,
   document.getElementById('root')
 );
 
