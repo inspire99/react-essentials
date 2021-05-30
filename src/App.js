6742b7eccceb172724d1132c05d5931b3bb6d136
TECHNO PARADIGMS <ganeshswamypillai@gmail.com>
2021-05-30 16:08:04 +0530

    using redurer to return new state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { useState} from 'react';
+import React, { useReducer, useState} from 'react';
 import './App.css';
 
 function App({authorized}) {
 
-  const [checked, setChecked] = useState(false);
-
-  function toggle(){
-    setChecked( (checked) => !checked );
-  }
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false);
 
   return(
     <>
